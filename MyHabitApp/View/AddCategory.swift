5c1c91868db30e23e10bb033e6c65dbb8c12a7d3
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-24 12:56:46 +0530

    color and systemic call published in categorytaskviewmodel and call it addcategoryscreen

--- MyHabitApp/View/AddCategory.swift
+++ MyHabitApp/View/AddCategory.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct AddCategory: View {
-    @State private var color = Color.indigo
-    @State private var systemIcon = "calendar"
+//    @State private var color = Color.indigo
+//    @State private var systemIcon = "calendar"
+    @EnvironmentObject var categoryModel: CategoryTaskViewModel
     private let systemIcons = [
         "calendar", "book.closed", "bookmark", "graduationcap", "cart",
         "display", "clock", "network", "phone", "heart"
@@ -19,8 +20,17 @@ struct AddCategory: View {
             ScrollView{
                 VStack(spacing: 20){
                     VStack(alignment: .center, spacing: 20){
-                        CategoryIconView(systemIcon: systemIcon, color: color, size: .lg)
+                        CategoryIconView(systemIcon: categoryModel.systemIcon, color: categoryModel.color, size: .lg)
+                        TextField("Title", text: $categoryModel.categoryTitle)
+                            .padding(10)
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(10)
                     }
+                    .padding()
+                    .background(Color(UIColor.systemGray5))
+                    .cornerRadius(10)
+                  //  LazyVGrid(columns: columns, spacing: 20)
+                    
                 }
             }
         }
@@ -30,5 +40,6 @@ struct AddCategory: View {
 struct AddCategory_Previews: PreviewProvider {
     static var previews: some View {
         AddCategory()
+            .environmentObject(CategoryTaskViewModel())
     }
 }
--- MyHabitApp/ViewModel/CategoryTaskViewModel.swift
+++ MyHabitApp/ViewModel/CategoryTaskViewModel.swift
@@ -5,3 +5,11 @@
 ////  Created by Bhumika Patel on 18/01/23.
 ////
 //
+import SwiftUI
+import CoreData
+
+class CategoryTaskViewModel: ObservableObject{
+    @Published var categoryTitle: String = ""
+    @Published  var color = Color.indigo
+    @Published  var systemIcon = "calendar"
+}
