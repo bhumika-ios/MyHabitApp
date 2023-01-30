2d1f3ad8fcfab4fe6285af676f150c65735e5bfe
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-30 11:58:54 +0530

    create task list data - formate used for display task data

---
+++ MyHabitApp/Screen/TaskListData.swift
@@ -0,0 +1,45 @@
+//
+//  TaskListData.swift
+//  MyHabitApp
+//
+//  Created by Bhumika Patel on 30/01/23.
+//
+
+import SwiftUI
+
+struct TaskListData: View {
+    @State private var searchValue = ""
+    @State private var filteredByCategory: Category? = nil
+    @State private var isAddCategoryOpen = false
+    @State private var isAddTaskOpen = false
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                SectionTitleView(title: "")
+                
+                TaskList(query: searchValue, category: filteredByCategory)
+            }
+        
+        .padding(.leading)
+        .padding(.trailing)
+        .searchable(text: $searchValue, placement: .toolbar)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem (placement: .primaryAction) {
+                Button(action: { isAddTaskOpen = true }) {
+                    Image(systemName: "plus.circle")
+                        .sheet(isPresented: $isAddTaskOpen) {
+                            AddTaskScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct TaskListData_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskListData()
+    }
+}
