28108a0a1d2152587e16d618da07690b0ea58af5
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-02-03 12:24:50 +0530

    Habit for create addAbbit screen - create field key name and edit for used for connect to category screen

--- MyHabitApp/Habit/AddHabitScreen.swift
+++ MyHabitApp/Habit/AddHabitScreen.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 
+
 struct AddHabitScreen: View {
+  //  @EnvironmentObject var habitModel: HabitViewModel
     @Environment(\.managedObjectContext) private var moc
     @Environment(\.dismiss) private var dismiss
     @Environment(\.self) var env
@@ -18,8 +20,10 @@ struct AddHabitScreen: View {
     
     @State private var title = ""
     @State private var remainderText = ""
+    @State private var weekDays1 = ["Monday"]
     @State private var category: UUID?
     @State private var dateAdded = Date()
+    @State private var isRemainderOn = false
     @State private var isAddCategoryOpen = false
     @State private var backtoHome = false
     
@@ -30,6 +34,7 @@ struct AddHabitScreen: View {
             self.habit = safeHabit
             self._title = .init(initialValue: safeHabit.title!)
             self._remainderText = .init(initialValue: safeHabit.remainderText!)
+            self._weekDays1 = .init(initialValue: safeHabit.weekDays!)
             self._dateAdded = .init(initialValue: safeHabit.dateAdded!)
             self._category = .init(initialValue: safeHabit.category!.id)
         }
@@ -45,12 +50,13 @@ struct AddHabitScreen: View {
         if habit != nil {
             habit?.title = title
             habit?.remainderText = remainderText
+            habit?.weekDays = weekDays1
             habit?.category = selectedCategory
             habit?.dateAdded = dateAdded
             
             PersistenceController.shared.save(context: moc)
         } else {
-            PersistenceController.shared.createHabit(context: moc, category: selectedCategory!, title: title, remainderText: remainderText ,dateAdded: dateAdded )
+            PersistenceController.shared.createHabit(context: moc, category: selectedCategory!, title: title, remainderText: remainderText ,dateAdded: dateAdded, weekDays: weekDays1 )
         }
         
        // dismiss()
@@ -58,12 +64,156 @@ struct AddHabitScreen: View {
           
     }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Habit Name", text: $title)
+                TextField("RemainText", text: $remainderText)
+                HStack(spacing: 20){
+                  //  HStack{
+                        Picker("Select Category", selection: $category) {
+                         //   HStack{
+                                ForEach(categories) {
+                                    
+                                    Text($0.title!).tag($0.id)
+                
+                                //    CategoryIconView(systemIcon: $0.systemIcon! , color: $0.color != nil ? Color(hex: $0.color!)! : Color.pink).tag($0.id)
+                             //   }
+                            }
+                          //  .padding()
+                        }
+                       
+                        
+                   // }
+                    
+                }
+                VStack(alignment: .leading, spacing: 6){
+                    Text("Frequency")
+                        .font(.callout.bold())
+                    var weekDays = Calendar.current.weekdaySymbols
+                    HStack(spacing: 10){
+                        ForEach(weekDays,id: \.self){day in
+                            let index = weekDays1.firstIndex { value in
+                                return value == day
+                            } ?? -1
+                            //Mark: Limiting to first 2 latters
+                            Text(day.prefix(2))
+//                                .font(.system(size: 14))
+                                .fontWeight(.semibold)
+                               .frame(maxWidth: .infinity)
+//                                .padding()
+                                .background{
+                                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                        .fill(index != -1 ? Color("Pink") : .gray).opacity(0.5)
+                                       // .fill(index != -1 ? Color(categories.color) : Color("TFBG").opacity(0.4))
+                                }
+                                .onTapGesture{
+                                    withAnimation{
+                                        if index != -1{
+                                            weekDays1.remove(at: index)
+                                        }else{
+                                            weekDays1.append(day)
+                                          
+                                        }
+                                    }
+                                }
+                            
+                        }
+                    }
+                    .padding(.top, 15)
+                }
+                HStack{
+                    VStack(alignment: .leading, spacing: 6){
+                        Text("Remainder")
+                            .fontWeight(.semibold)
+                        Text("Just notification")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Toggle(isOn: $isRemainderOn){
+                        
+                    }
+                    .labelsHidden()
+                }
+                
+                HStack{
+                   
+                    DatePicker(
+                        "Do Date",
+                        selection: $dateAdded,
+                        in: Date()...
+                    )
+                  //  Text(habitModel.dateAdded.formatted(date: .omitted, time: .shortened))
+                }
+               
+                .frame(height: isRemainderOn ? nil : 0)
+                .opacity(isRemainderOn ? 1 : 0)
+//                .animation(.easeInOut, value: habitModel.isRemainderOn)
+//                .frame(maxHeight: .infinity, alignment: .top)
+//                .padding()
+//                .navigationBarTitleDisplayMode(.inline)
+                
+            }
+            .toolbar {
+                ToolbarItem (placement: .navigationBarLeading) {
+                    Button (action: { dismiss() }) {
+                        Text("Cancel")
+                    }
+                }
+                
+              
+                ToolbarItem (placement: .navigationBarTrailing) {
+                    Button(action: { publishHabit()
+                        backtoHome = true
+                    })
+                           {
+                       Text("Done")
+//
+                        
+                    }
+                    .disabled(title == "")
+                        
+                    .fullScreenCover(isPresented: $backtoHome){
+                                        }content: {
+                                           DisplayHabitView()
+                                                //.environmentObject(taskModel)
+                                        }
+            
+//
+                }
+               
+            }
+            .navigationTitle("Habits")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+               // .overlay(alignment: .trailing){
+                    Button{
+                        if let editHabit =  habit {
+                            env.managedObjectContext.delete(editHabit)
+                            try? env.managedObjectContext.save()
+                            env.dismiss()
+                        }
+                        
+                        
+                    }label: {
+                        Image(systemName: "trash")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .font(.title3)
+                            .foregroundColor(.red)
+                    }
+                // data filled show delete icon else not show
+                    .opacity( habit == nil ? 0 : 1)
+               // }
+            }
+        }
     }
 }
 
 struct AddHabitScreen_Previews: PreviewProvider {
     static var previews: some View {
         AddHabitScreen()
+           // .environmentObject(HabitViewModel())
     }
 }
